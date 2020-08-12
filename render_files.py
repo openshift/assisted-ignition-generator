725a25aa4cddf357d2f55a85c90cbf8ed462178a
Nick Carboni <ncarboni@redhat.com>
2020-08-12 08:39:59 -0400

    MGMT-1790 Don't write to root only directories
    This doesn't seem to be used currently and breaks us when we're
    not running as root (i.e. in OpenShift)
    
    Fixes MGMT-1790

--- render_files.py
+++ render_files.py
@@ -139,7 +139,7 @@ def set_pull_secret(config_dir):
 # def prepare_generation_data(work_dir, config_dir, install_config, openshift_release_image):
 def prepare_generation_data(config_dir, install_config):
     prepare_install_config(config_dir, install_config)
-    set_pull_secret(config_dir)
+    # set_pull_secret(config_dir)
     # [TODO] - remove comment after fixing subsystem
     # oc_utils.extract_baremetal_installer(work_dir, openshift_release_image)
 
